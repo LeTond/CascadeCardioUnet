e1e01dcf5e72ed28b2dc0098d376f112ad251961
Anatoliy Levchuk <feuerlag999@yandex.ru>
2023-03-29 11:53:07 +0300

    added some TODO and files

---
+++ Training/dataset.py
@@ -0,0 +1,73 @@
+
+
+import torch
+import numpy as np
+import torchvision.transforms.functional as TF
+import torchvision.transforms as transforms
+
+
+from torch.utils.data import Dataset
+# from configuration import *
+
+
+########################################################################################################################
+##TODO: COMMENTS
+########################################################################################################################
+
+class MyDataset(Dataset):
+
+    def __init__(self, num_layers, ds_origin, ds_mask, ds_names, kernel_sz, transform = None, target_transform = None, images_and_labels = []):
+        self.target_transform = target_transform
+        self.transform = transform
+        self.images_and_labels = images_and_labels
+        self.images = ds_origin
+        self.masks = ds_mask
+        self.names = ds_names
+        self.kernel_sz = kernel_sz
+        self.num_layers = num_layers
+
+        for i in range(len(self.images)):
+            self.images_and_labels.append((i, i, i))
+
+    def preprocessing(self, image, label):
+
+        label = label / 3
+        image = TF.to_pil_image(image)
+        label = TF.to_pil_image(label)
+        image = TF.pil_to_tensor(image)
+        label = TF.pil_to_tensor(label)
+
+        tcat = torch.cat((image, label), 0)
+        image, label = self.transform(tcat)
+
+        Normalize_tr = transforms.Compose([transforms.ToPILImage(),transforms.ToTensor(), transforms.Normalize(0.5,0.5)])
+        image = Normalize_tr(image)
+
+        image = np.array(image.reshape(self.kernel_sz, self.kernel_sz, 1), dtype=np.float32)
+        label = np.array(label.reshape(self.kernel_sz, self.kernel_sz, 1), dtype=np.float32)
+
+        label = label * 3
+
+        return image, label
+
+    def __getitem__(self, item):
+        imgs, labs, sub_nms = self.images_and_labels[item]
+        image = self.images[imgs][:][:]
+        label = self.masks[labs][:][:]
+        sub_names = self.names[sub_nms]
+
+        image, label = self.preprocessing(image, label)
+
+        image = image.transpose(2, 0, 1) 
+        label = np.resize(label, (self.kernel_sz, self.kernel_sz))
+        label = np.array(label, dtype=np.int8)
+        label = np.eye(self.num_layers)[label]    # 0 - background; 1 - left ventricle; 2 - myocardium; 3 - fibrosis   
+        label = np.array(label, dtype=np.float32)
+        label = label.transpose(2, 0, 1)
+
+        return image, label, sub_names
+
+    def __len__(self):
+        return len(self.images)
+
+
